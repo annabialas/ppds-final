b9a2e66700ea20cc530fc955eb88fc80171ad064
annabialas <ab5086@nyu.edu>
2017-05-08 18:44:24 -0400

    int

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
 	# 	app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
 	# else:
 	# 	app.run(host='localhost', port=5000, debug=True)
-	app.run(host="0.0.0.0", port=os.environ.get("PORT"))
+	app.run(host="0.0.0.0", port=int(os.environ.get("PORT")))
